3b933853ce829f47c8a586f66a2c98e574b80749
Poruri Sai Rahul <rporuri@enthought.com>
2017-10-29 16:29:09 +0530

    DEV : use __name__/__main__

--- classifier/plot_iris_dataset.py
+++ classifier/plot_iris_dataset.py
@@ -49,46 +49,47 @@ def load_data(data_file_name):
     return data, target, target_names
 
 
-data, target, target_names = load_data('iris.csv')
+if __name__ == "__main__":
+    data, target, target_names = load_data('iris.csv')
 
-iris = dict(data=data, target=target,
-                 target_names=target_names,
-                 feature_names=['sepal length (cm)', 'sepal width (cm)',
-                                'petal length (cm)', 'petal width (cm)'])
+    iris = dict(data=data, target=target,
+                    target_names=target_names,
+                    feature_names=['sepal length (cm)', 'sepal width (cm)',
+                                    'petal length (cm)', 'petal width (cm)'])
 
-X = iris['data'][:, :2]  # we only take the first two features.
-y = iris['target']
+    X = iris['data'][:, :2]  # we only take the first two features.
+    y = iris['target']
 
-x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 
-plt.figure(2, figsize=(8, 6))
-plt.clf()
+    plt.figure(2, figsize=(8, 6))
+    plt.clf()
 
-# Plot the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1,
-            edgecolor='k')
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
+    # Plot the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1,
+                edgecolor='k')
+    plt.xlabel('Sepal length')
+    plt.ylabel('Sepal width')
 
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
 
-# To getter a better understanding of interaction of the dimensions
-# plot the first three PCA dimensions
-fig = plt.figure(1, figsize=(8, 6))
-ax = Axes3D(fig, elev=-150, azim=110)
-X_reduced = PCA(n_components=3).fit_transform(iris['data'])
-ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y,
-           cmap=plt.cm.Set1, edgecolor='k', s=40)
-ax.set_title("First three PCA directions")
-ax.set_xlabel("1st eigenvector")
-ax.w_xaxis.set_ticklabels([])
-ax.set_ylabel("2nd eigenvector")
-ax.w_yaxis.set_ticklabels([])
-ax.set_zlabel("3rd eigenvector")
-ax.w_zaxis.set_ticklabels([])
+    # To getter a better understanding of interaction of the dimensions
+    # plot the first three PCA dimensions
+    fig = plt.figure(1, figsize=(8, 6))
+    ax = Axes3D(fig, elev=-150, azim=110)
+    X_reduced = PCA(n_components=3).fit_transform(iris['data'])
+    ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y,
+            cmap=plt.cm.Set1, edgecolor='k', s=40)
+    ax.set_title("First three PCA directions")
+    ax.set_xlabel("1st eigenvector")
+    ax.w_xaxis.set_ticklabels([])
+    ax.set_ylabel("2nd eigenvector")
+    ax.w_yaxis.set_ticklabels([])
+    ax.set_zlabel("3rd eigenvector")
+    ax.w_zaxis.set_ticklabels([])
 
-plt.show()
+    plt.show()
